0a5ba74b4b1260171784d214276eb8d762c8ba59
Jon Brelig <jonbrelig@gmail.com>
2015-07-26 12:20:04 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='SurveyGizmo',
-    version=__version__,
+    version="1.0.1",
     description='A Python Wrapper for SurveyGizmo\'s restful API service.',
     long_description=README,
     author='Ryan P Kilby',
